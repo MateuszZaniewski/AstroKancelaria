2362f8722636760328860c7e58af3b6e0c8488aa
MateuszZaniewski <mateusz.zaniewski94@gmail.com>
2024-09-26 19:16:55 +0200

    back to netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,56 +1,19 @@
-// import { defineConfig } from "astro/config";
-// import netlify from "@astrojs/netlify";
-// import compression from "vite-plugin-compression";
-// import react from "@astrojs/react";
-// import tailwind from "@astrojs/tailwind";
-// import sitemap from "@astrojs/sitemap";
-
-// import compress from "astro-compress";
-
-// // https://astro.build/config
-// export default defineConfig({
-//   site: "https://kancelaria-paulinakomenda.netlify.app/",
-//   integrations: [react(), tailwind(), sitemap(), compress()],
-//   vite: {
-//     plugins: [compression()],
-//   },
-//   output: "server",
-//   adapter: netlify(),
-// });
-
-// import { defineConfig } from "astro/config";
-// import cloudflare from "@astrojs/cloudflare"; // Use the Cloudflare adapter
-// import compression from "vite-plugin-compression";
-// import react from "@astrojs/react";
-// import tailwind from "@astrojs/tailwind";
-// import sitemap from "@astrojs/sitemap";
-// import compress from "astro-compress";
-
-// // https://astro.build/config
-// export default defineConfig({
-//   site: "https://f200a3b4.astrokancelaria.pages.dev/", // Update to Cloudflare Pages URL
-//   integrations: [react(), tailwind(), sitemap(), compress()],
-//   vite: {
-//     plugins: [compression()],
-//   },
-//   output: "server", // Use static output for Cloudflare Pages
-//   adapter: cloudflare(), // Use Cloudflare Pages adapter
-// });
-
 import { defineConfig } from "astro/config";
-import cloudflare from "@astrojs/cloudflare";
+import netlify from "@astrojs/netlify";
 import compression from "vite-plugin-compression";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
+
 import compress from "astro-compress";
 
+// https://astro.build/config
 export default defineConfig({
-  site: "https://a1206423.astrokancelaria.pages.dev/",
+  site: "https://kancelaria-paulinakomenda.netlify.app/",
   integrations: [react(), tailwind(), sitemap(), compress()],
   vite: {
     plugins: [compression()],
   },
-  output: "static", // Static output
-  // Use static adapter for development, cloudflare for production
+  output: "server",
+  adapter: netlify(),
 });
